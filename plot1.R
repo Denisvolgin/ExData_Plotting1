7f0feed18d5b5e3497e1e5dc9b994f97bd36d8f9
Denis Volgin <denisvolgin@gmail.com>
2014-06-08 15:42:28 -0400

    plot1.R

---
+++ plot1.R
@@ -0,0 +1,20 @@
+#load data
+loadHouseholdPowerConsumptionData <- function(path) {
+  data <- read.csv(path, sep=";", na.strings="?")
+  data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+  data <- subset(data, (Date >= "2007-02-01" & Date <= "2007-02-02"))
+  data$Datetime <- as.POSIXct(datetime <- paste(as.Date(data$Date), data$Time))
+  
+  return(data)
+}
+
+#draw histogram
+plot1 <- function(data, png.filename) {
+  hist(data$Global_active_power, main="Global Active Power",
+       xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+  
+  dev.copy(png, file=png.filename, height=480, width=480)
+  dev.off()
+}
+
+plot1(loadData("household_power_consumption.txt"), "plot1.png")
